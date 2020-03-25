4a7a5abf04c12cba2d535b97aba0b694905e431f
ijuresa <ivanjuresa611@gmail.com>
2020-03-25 07:49:00 +0100

    MASTER: Init commit

---
+++ stm32l4/project_settings/typedefs.h
@@ -0,0 +1,77 @@
+/***********************************************************************************************//**
+ * MIT License
+ * 
+ * Copyright (c) 2020 ivan.juresa
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ **************************************************************************************************
+ * @file   typedefs.h
+ * @author ivan.juresa
+ * @brief  
+ **************************************************************************************************/
+
+#ifndef TYPEDEFS_H_
+#define TYPEDEFS_H_
+
+/***************************************************************************************************
+ *                      INCLUDE FILES
+ **************************************************************************************************/
+
+/***************************************************************************************************
+ *                      DEFINES
+ **************************************************************************************************/
+#define FALSE (0u)
+#define TRUE  (1u)
+
+/***************************************************************************************************
+ *                      ENUMERATIONS
+ **************************************************************************************************/
+
+/***************************************************************************************************
+ *                      UNIONS
+ **************************************************************************************************/
+
+/***************************************************************************************************
+ *                      DATA TYPES
+ **************************************************************************************************/
+// Float
+typedef float fp32_t;
+typedef volatile float vfp32_t;
+
+// Double
+typedef double fp64_t;
+typedef volatile double vfp64_t;
+
+/***************************************************************************************************
+ *                      DATA STRUCTURES
+ **************************************************************************************************/
+
+/***************************************************************************************************
+ *                      GLOBAL VARIABLES DECLARATIONS
+ **************************************************************************************************/
+
+/***************************************************************************************************
+ *                      PUBLIC FUNCTION PROTOTYPES
+ **************************************************************************************************/
+
+#endif /* TYPEDEFS_H_ */
+
+/***************************************************************************************************
+ *                      END OF FILE
+ **************************************************************************************************/
